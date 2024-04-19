f5f3bf314b429650ef90aa4926f3355e74eca998
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2024-04-19 14:29:10 +0700

    update code

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const morgan = require("morgan");
+const ModbusRTU = require("modbus-serial");
+const client = new ModbusRTU();
+
+client
+  .connectTCP("192.168.10.33", { port: 502 })
+  .then(() => {
+    console.log("Connected to PLC");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to PLC:", error);
+  });
 
 app.use(morgan("combined"));
 
-app.get("/chungngngalta2", (req, res) => {
+app.get("/search", (req, res) => {
   var a = 1;
   var b = 2;
   var c = a + b;
 
-  return res.send("Hello World Chung Nguyen Ngoc!");
+  res.send("<h1>Chung NguyeNgoc</h1>");
 });
 
 app.listen(port, () => {
